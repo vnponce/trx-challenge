277e299b5d27cc6a5a8b9ef140f2bfcf0e3690a0
abel <vnponce8@gmail.com>
2024-02-15 12:41:23 -0600

    feat(plugins): adds vueQuery, msw and tailwind

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import { VueQueryPlugin } from 'vue-query'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(VueQueryPlugin);
+
 
 app.mount('#app')
