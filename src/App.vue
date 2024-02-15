34e947c008c7c49b5955da26c5907b5d5d3fcd08
abel <vnponce8@gmail.com>
2024-02-15 12:31:01 -0600

    feat(init): Vue project initialized

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import UserList from '@/components/UserList.vue'
+</script>
+
+<template>
+  <header>
+    <!--    Create input search user list -->
+<!--    <Search />-->
+  </header>
+
+  <main>
+    <!--    User list -->
+    <UserList  :users="[{ id: 1, name: 'John Doe', email: 'john@traxporta.com'}]" />
+  </main>
+</template>
