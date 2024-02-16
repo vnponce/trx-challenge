c6612b0c23c5a14e515ad94a647db8ce7cf56b76
abel <vnponce8@gmail.com>
2024-02-16 00:41:59 -0600

    feat(App): Aside component

---
+++ src/components/layout/Aside.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div class="pb-2 pt-2 md:pt-10 w-full md:w-[95px] max-h-full border-r-gray-100 border-2">
+    <img
+      src="/traxporta-logo.png"
+      alt="Traxporta logo"
+      width="50"
+      height="50"
+      class="m-auto shadow-md rounded-xl"
+    />
+  </div>
+</template>
