aab583c2a7617e9b9cd75755ee2acb37c16a7f80
abel <vnponce8@gmail.com>
2024-02-16 00:52:19 -0600

    feat(App): show `no match items` if there's no user to show

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -10,25 +10,31 @@ defineProps({
 </script>
 
 <template>
-  <div
-    class="px-10 grid grid-cols-[60px_1fr_1fr] grid-cols-3 font-light text-xs text-stone-400 mt-6 mb-4"
-  >
+  <div class="px-10 grid grid-cols-[60px_1fr_1fr] font-light text-xs text-stone-400 mt-6 mb-4">
     <div></div>
     <div>Name</div>
     <div>E-mail</div>
   </div>
-  <div class="border-t border-stone-200/90 divide-y divide-stone-200/90">
+  <div>
     <div
-      v-for="{ name, email, id } in users"
-      :key="id"
-      class="grid grid-cols-[60px_1fr_1fr] grid-cols-3 py-6 px-10 text-xs font-bold]"
+      v-if="users.length === 0"
+      class="border-t border-stone-200/90 divide-y divide-stone-200/90 py-6 px-10 text-xs font-bold"
     >
-      <Avatar :name="name" :id="id" />
-      <div class="flex items-center">
-        {{ name }}
-      </div>
-      <div class="flex items-center">
-        {{ email }}
+      No items to match
+    </div>
+    <div class="border-t border-stone-200/90 divide-y divide-stone-200/90" v-else>
+      <div
+        v-for="{ name, email, id } in users"
+        :key="id"
+        class="grid grid-cols-[60px_1fr_1fr] py-6 px-10 text-xs font-bold"
+      >
+        <Avatar :name="name" :id="id" />
+        <div class="flex items-center">
+          {{ name }}
+        </div>
+        <div class="flex items-center">
+          {{ email }}
+        </div>
       </div>
     </div>
   </div>
--- src/components/__tests__/UserList.spec.js
+++ src/components/__tests__/UserList.spec.js
@@ -7,11 +7,19 @@ import { screen } from '@testing-library/vue'
 describe('User List', () => {
   it('renders properly', async () => {
     const users = [{ id: 1, name: 'John Doe', email: 'john@traxporta.com' }]
-    const wrapper = render(UserList, { props: { users } })
+    render(UserList, { props: { users } })
 
     const nameElement = await screen.findByText(/John Doe/i)
     expect(nameElement).toBeInTheDocument()
     const emailElement = await screen.findByText(/john@traxporta.com/i)
     expect(emailElement).toBeInTheDocument()
+  })
+
+  it('renders properly when receives an empty array', async () => {
+    const users = []
+    render(UserList, { props: { users } })
+
+    const emptyElement = await screen.findByText(/No items to match/i)
+    expect(emptyElement).toBeInTheDocument()
   })
 })
