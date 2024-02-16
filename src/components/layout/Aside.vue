8605054b93ce7ee27dd48a58b64c23d2a89dbc98
abel <vnponce8@gmail.com>
2024-02-16 01:30:08 -0600

    fix(style): :lipstick: aside with only right border

--- src/components/layout/Aside.vue
+++ src/components/layout/Aside.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="pb-2 pt-2 md:pt-10 w-full md:w-[95px] max-h-full border-r-gray-100 border-2">
+  <div class="pb-2 pt-2 md:pt-10 w-full md:w-[95px] max-h-full border-r-2 border-r-gray-100">
     <img
       src="/traxporta-logo.png"
       alt="Traxporta logo"
